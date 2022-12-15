9c8dffba99897831b65c664f4ec15391b648cf0c
JustSong <songquanpeng@foxmail.com>
2022-12-15 19:46:19 +0800

    :tada: delete action won't be saved as post anymore

--- controller.go
+++ controller.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
@@ -46,7 +47,7 @@ func GetNonsenseById(c *gin.Context) {
 	} else {
 		c.JSON(http.StatusOK, gin.H{
 			"success": true,
-			"message": "okay",
+			"message": "",
 			"data":    nonsense,
 		})
 	}
@@ -66,12 +67,14 @@ func GetNonsense(c *gin.Context) {
 			"status":  false,
 			"message": err.Error(),
 		})
+		return
 	} else {
 		c.JSON(http.StatusOK, gin.H{
 			"status":  true,
-			"message": "okay",
+			"message": "",
 			"data":    nonsenses,
 		})
+		return
 	}
 
 }
@@ -80,98 +83,106 @@ func PostNonsense(c *gin.Context) {
 	var postRequest PostRequest
 	err := json.NewDecoder(c.Request.Body).Decode(&postRequest)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"success": false,
-			"message": "Invalid parameter",
+			"message": "无效的参数",
 		})
 		return
 	}
-	if *Token == postRequest.Token {
-		// enable user to delete nonsense by type `delete id`
-		if strings.HasPrefix(postRequest.Content, "delete ") {
-			t := strings.Split(postRequest.Content, " ")
-			idStr := t[len(t)-1]
-			id, err := strconv.Atoi(idStr)
-			if err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{
-					"success": false,
-					"message": "Invalid parameter",
-				})
-				return
-			} else {
-				var nonsenseObj Nonsense
-				DB.Where("id = ?", id).First(&nonsenseObj)
-				err := nonsenseObj.Delete()
-				if err != nil {
-					c.JSON(http.StatusBadRequest, gin.H{
-						"success": false,
-						"message": "Failed to delete",
-					})
-					return
-				}
-			}
-		}
-
-		nonsenseObj := &Nonsense{
-			Content: postRequest.Content,
-			Time:    time.Now().Format("2006-01-02 15:04:05"),
-		}
-		err := nonsenseObj.Insert()
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"success": true,
-				"message": err.Error(),
-			})
-		} else {
-			message := "Nonsense posted successfully."
-			c.JSON(http.StatusOK, gin.H{
-				"success": true,
-				"message": message,
-				"data":    nonsenseObj.Id,
-			})
-		}
-
-	} else {
+	if *Token != postRequest.Token {
 		c.JSON(http.StatusOK, gin.H{
 			"success": false,
-			"message": "Invalid token.",
+			"message": "无效的凭证",
 		})
+		return
 	}
+	// Enable user to delete nonsense by type `delete id`
+	if strings.HasPrefix(postRequest.Content, "delete ") {
+		t := strings.Split(postRequest.Content, " ")
+		idStr := t[len(t)-1]
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"success": false,
+				"message": "无效的参数",
+			})
+			return
+		}
+		var nonsenseObj Nonsense
+		DB.Where("id = ?", id).First(&nonsenseObj)
+		if nonsenseObj.Id == 0 {
+			c.JSON(http.StatusOK, gin.H{
+				"success": false,
+				"message": fmt.Sprintf("无效的微博 ID: %s", idStr),
+			})
+			return
+		}
+		err = nonsenseObj.Delete()
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"success": false,
+				"message": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"success": true,
+			"message": "",
+		})
+		return
+	}
+	nonsenseObj := &Nonsense{
+		Content: postRequest.Content,
+		Time:    time.Now().Format("2006-01-02 15:04:05"),
+	}
+	err = nonsenseObj.Insert()
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": "",
+		"data":    nonsenseObj.Id,
+	})
+	return
 }
 
 func DeleteNonsense(c *gin.Context) {
 	var deleteRequest DeleteRequest
 	err := json.NewDecoder(c.Request.Body).Decode(&deleteRequest)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"success": false,
-			"message": "Invalid parameter",
+			"message": "无效的参数",
 		})
 		return
 	}
-	if *Token == deleteRequest.Token {
-		nonsenseObj := &Nonsense{
-			Id: deleteRequest.Id,
-		}
-		DB.Where("id = ?", deleteRequest.Id).First(&nonsenseObj)
-		err := nonsenseObj.Delete()
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"success": true,
-				"message": err.Error(),
-			})
-		} else {
-			message := "Nonsense deleted successfully."
-			c.JSON(http.StatusOK, gin.H{
-				"success": true,
-				"message": message,
-			})
-		}
-
-	} else {
+	if *Token != deleteRequest.Token {
 		c.JSON(http.StatusOK, gin.H{
 			"success": false,
-			"message": "Invalid token.",
+			"message": "无效的凭证",
 		})
+		return
 	}
+	nonsenseObj := &Nonsense{
+		Id: deleteRequest.Id,
+	}
+	DB.Where("id = ?", deleteRequest.Id).First(&nonsenseObj)
+	err = nonsenseObj.Delete()
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": "",
+	})
+	return
 }
